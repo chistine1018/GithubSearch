224fdb0fc811b95ec4355f22def09ed77d587e7b
Anson <chistine101814@gmail.com>
2023-04-12 16:08:21 +0800

    建立基本MVVM+Data Binding架構

--- app/src/main/java/com/github/MainActivity.java
+++ app/src/main/java/com/github/MainActivity.java
@@ -1,14 +1,24 @@
 package com.github;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    private MainViewModel viewModel = new MainViewModel();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setViewModel(viewModel);
     }
 }
